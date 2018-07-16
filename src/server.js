5ffa73be6db9828b6ccdb3d74c7efa666032b845
Alameen688 <ogundiran12@gmail.com>
2018-07-16 15:52:26 +0100

    update server file

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
-const router = express.Router();
+const app = express();
+app.use(bodyParser);
 
+app.use(morgan('dev'));
+
+app.listen(process.env.PORT || 300, () => {
+  console.log(`Listening on port ${this.address().port}`);
+});

c2fb14b7f1563b6ef0131b7e0b1be966ceea6a87
Alameen688 <ogundiran12@gmail.com>
2018-07-20 05:33:58 +0100

    allow cross origin resource sharing

--- dist/server.js
+++ dist/server.js
@@ -29,6 +29,13 @@ app.use((0, _morgan2.default)('dev'));
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use(_bodyParser2.default.json());
 
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-type, Accept, X-Access-Token, X-Key');
+  next();
+});
+
 app.use('/api/v1/entries', _index2.default.entries);
 
 // when there is no fitting route set error and run the next func
